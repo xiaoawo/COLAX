065fbc64e66717157cd101610c44377f4104cf9d
小蜗 <7466492+xiaowo_xd@user.noreply.gitee.com>
2024-09-08 18:40:01 +0800

    refactor(all): module调整结束

--- colax-components/colax-component-catchlog-starter/src/main/java/com/alibaba/cola/catchlog/ResponseHandlerI.java
+++ colax-components/colax-component-catchlog-starter/src/main/java/com/alibaba/cola/catchlog/ResponseHandlerI.java
@@ -1,6 +1,5 @@
 package com.alibaba.cola.catchlog;
 
-import com.alibaba.cola.exception.BaseException;
 
 public interface ResponseHandlerI {
     public Object handle(Class returnType, String errCode, String errMsg);
