182905ec8644fc84c60d822e91ffb6ce5bab6f64
小蜗 <7466492+xiaowo_xd@user.noreply.gitee.com>
2024-09-12 20:57:08 +0800

    feat(dto-id): 添加ID和ShardingID泛型类
    在dto模块中引入新的ID处理方案，包含ID和ShardingID两个泛型类，支持分布式系统中的唯一标识管理。

---
+++ colax-components/colax-component-dto/src/main/java/com/alibaba/cola/dto/id/ShardingID.java
@@ -0,0 +1,25 @@
+package com.alibaba.cola.dto.id;
+
+import java.util.Objects;
+
+/*** 分布式ID的基础类
+ * @author 小蜗
+ * @version 1.0.0
+ */
+public class ShardingID<S, V> extends ID<V> {
+	private final S sharding;
+
+	public ShardingID(S sharding, V value) {
+		super(value);
+		Objects.requireNonNull(sharding, "sharding must not be null");
+		this.sharding = sharding;
+	}
+
+	public S sharding() {
+		return sharding;
+	}
+
+	public String toString() {
+		return String.format("{\"sharding\": %s, \"value\": %s}", sharding, value());
+	}
+}
